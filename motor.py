4f7e7ef16d4dfc45e76f301fa1f2fbff7d7eb8b7
spsanps <sanjayanps@gmail.com>
2016-10-19 18:25:13 +0530

    Small change to account for out of range values on the ultrasonic sensors

--- motor.py
+++ motor.py
@@ -1,47 +1,48 @@
-
-from time import time, sleep
-
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
+
 class Motor:
-    def __init__(self,values):
+    def __init__(self, values):
         self.speed = values[0]
         self.input1 = values[1]
         self.input2 = values[2]
-        GPIO.setup(self.input1,GPIO.OUT)
-        GPIO.setup(self.input2,GPIO.OUT)
-        GPIO.setup(self.speed,GPIO.OUT)
-    
-def front(motor1,motor2):
+        GPIO.setup(self.input1, GPIO.OUT)
+        GPIO.setup(self.input2, GPIO.OUT)
+        GPIO.setup(self.speed, GPIO.OUT)
+
+
+def front(motor1, motor2):
     GPIO.output(motor1.input1, False)
     GPIO.output(motor2.input1, False)
     GPIO.output(motor1.input2, True)
     GPIO.output(motor2.input2, True)
 
-def back(motor1,motor2):
+
+def back(motor1, motor2):
     GPIO.output(motor1.input1, True)
     GPIO.output(motor2.input1, True)
     GPIO.output(motor1.input2, False)
     GPIO.output(motor2.input2, False)
 
 
-def turnLeft(motor1,motor2):
+def turnLeft(motor1, motor2):
     GPIO.output(motor1.input1, True)
     GPIO.output(motor2.input1, False)
     GPIO.output(motor1.input2, False)
     GPIO.output(motor2.input2, True)
 
-def turnRight(motor1,motor2):
+
+def turnRight(motor1, motor2):
     GPIO.output(motor1.input1, False)
     GPIO.output(motor2.input1, True)
     GPIO.output(motor1.input2, True)
     GPIO.output(motor2.input2, False)
 
-def stop(motor1,motor2):
+
+def stop(motor1, motor2):
     GPIO.output(motor1.input1, False)
     GPIO.output(motor2.input1, False)
     GPIO.output(motor1.input2, False)
     GPIO.output(motor2.input2, False)
-
