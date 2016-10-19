47143b344e691fde16dae8e4f12e838ce6e71409
spsanps <sanjayanps@gmail.com>
2016-10-19 23:48:45 +0530

    Run motor.py for motor testing

--- main2.py
+++ main2.py
@@ -1,26 +1,64 @@
-import math
+import serial
+import serial.tools.list_ports
 
 import motor
 from adapted import UltrasonicSensors
 
 trigger_pins = [23, 24, 25, 12, 16, 20]  # physical 16,18,22,32,36,38
 echo_pins = [4, 7, 27, 22, 5, 6]  # 7,11,13,15,29,31
+m1 = motor.Motor([18, 21, 13])  # physical 12,40,33
+m2 = motor.Motor([8, 19, 26])  # physical 24,35,37
 
-memory = 100  # How many old values of sensor to store
+FR = 0
+FL = 1
+BR = 2
+BL = 3
+F = 4
+B = 5
+
+clearance = 30
+
+sensor = UltrasonicSensors(trigger_pins, echo_pins)
+
+ports = list(serial.tools.list_ports.comports())
+
+port_loc = None
+
+for p in ports:
+    if "2341" in p[2].lower():
+        port_loc = p[0]
+
+        print "Arduino Found!"
+        break
+
+if port_loc is None:
+    raise Exception("Arduino not found.")
+
+ser = serial.Serial(port_loc)
+
+print "Connected to Arduino"
 
 
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+def loop():
+    read_serial = ser.readline().strip()
+
+    sensor_data, solar_panel_data = read_serial.split(';')
+
+    print "Incident Radiation: ", sensor_data[-1:]
+    print sensor_data
 
 
-def map_to_1(distance):
-    distance *= 2 / 100  # approx give 1 on sigmoid for distances 2.5m +
-    return 2 - sigmoid(distance)  # give 1 for near 0 values and
+while True:
 
+    distance = [sensor.distance(i) for i in xrange(6)]
+    for i in xrange(6):
+        if distance[i] is None:
+            distance[i] = 300
 
-class AutoPilot:
-    def __init__(self):
-        self.m1 = motor.Motor([18, 21, 13])  # physical 12,40,33
-        self.m2 = motor.Motor([8, 19, 26])  # physical 24,35,37
-        self.sensor = UltrasonicSensors(trigger_pins, echo_pins)
-        self.old_values = [[0 for i in xrange(100)] for j in xrange(6)]
+    min_d = min(distance)
+
+    if distance[FL] == min_d:
+        if distance[F] >= distance[B]:
+            pass
+
+    loop()
--- motor2.py
+++ motor2.py
@@ -27,16 +27,30 @@ def back(motor1, motor2):
     GPIO.output(motor2.input2, False)
 
 
-def turnLeft(motor1, motor2):
-    GPIO.output(motor1.input1, True)
+def turnLeftForward(motor1, motor2):
+    GPIO.output(motor1.input1, False)
     GPIO.output(motor2.input1, False)
     GPIO.output(motor1.input2, False)
     GPIO.output(motor2.input2, True)
 
 
-def turnRight(motor1, motor2):
+def turnLeftBackward(motor1, motor2):
     GPIO.output(motor1.input1, False)
-    GPIO.output(motor2.input1, True)
+    GPIO.output(motor2.input1, False)
+    GPIO.output(motor1.input2, True)
+    GPIO.output(motor2.input2, False)
+
+
+def turnRightForward(motor2, motor1):
+    GPIO.output(motor1.input1, False)
+    GPIO.output(motor2.input1, False)
+    GPIO.output(motor1.input2, False)
+    GPIO.output(motor2.input2, True)
+
+
+def turnRightBackward(motor2, motor1):
+    GPIO.output(motor1.input1, False)
+    GPIO.output(motor2.input1, False)
     GPIO.output(motor1.input2, True)
     GPIO.output(motor2.input2, False)
 
@@ -46,18 +60,3 @@ def stop(motor1, motor2):
     GPIO.output(motor2.input1, False)
     GPIO.output(motor1.input2, False)
     GPIO.output(motor2.input2, False)
-
-
-if __name__ == "__main__":
-    import time
-
-    m1 = Motor([18, 21, 13])  # physical 12, 40, 33
-    m2 = Motor([8, 19, 26])
-    turnRight(m1, m2)
-    time.sleep(2)
-    turnLeft(m1, m2)
-    time.sleep(2)
-    front(m1, m2)
-    time.sleep(2)
-    back(m1, m2)
-    time.sleep(2)
